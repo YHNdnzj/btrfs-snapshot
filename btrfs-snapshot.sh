8458155a9aefb7fa69e6da53a47c2bd92d47d909
Mike Yuan <me@yhndnzj.com>
2019-09-14 17:11:29 +0800

    Add num var

--- btrfs-snapshot.sh
+++ btrfs-snapshot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 subvol="$1"
+num=10
 date="$(date -I)"
 if [[ $subvol == / ]]; then
 	dest=/snapshot/root
@@ -11,7 +12,7 @@ else
 fi
 [[ -f $dest ]] && rm "$dest"
 [[ ! -e $dest ]] && mkdir -p "$dest"
-_num=$(( $(ls "$dest" | wc -l) - 9 ))
+_num=$(( $(ls "$dest" | wc -l) - num + 1 ))
 mapfile -t snapshot < <(ls -dr "$dest"/* | tail -n $_num)
 (( _num > 0 )) && btrfs subvolume delete "${snapshot[@]}"
 btrfs subvolume snapshot "$subvol" "$dest/$date"

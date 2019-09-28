a4bab611513706afbfe8dd3b9ceff964db7363c8
Mike Yuan <me@yhndnzj.com>
2019-09-28 16:10:34 +0800

    Reformat code

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ cd /tmp || exit 1
 wget -O btrfs-snapshot.zip https://github.com/YHNdnzj/btrfs-snapshot/archive/master.zip
 unzip btrfs-snapshot.zip
 cd btrfs-snapshot-master || exit 1
-sed -e 's|\(^_f_parseopt\)=.*|\1=/usr/local/lib/btrfs-snapshot/parseopt|' \
-    < btrfs-snapshot > btrfs-snapshot.sh
 install -Dm755 btrfs-snapshot.sh /usr/local/bin/btrfs-snapshot
+install -dm755 /usr/local/lib/btrfs-snapshot
 install -m644 parseopt /usr/lib/btrfs-snapshot/parseopt
+sed -e 's|\(^_f_parseopt\)=.*|\1=/usr/local/lib/btrfs-snapshot/parseopt|' \
+    < btrfs-snapshot > /usr/local/bin/btrfs-snapshot
 install -Dt /etc/systemd/system -m644 systemd/*
 cd ..
 rm -rf btrfs-snapshot{.zip,-master}

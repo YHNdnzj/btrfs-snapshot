a3c359b0c5e1b98fe0b96a7fdb3b70c38bffc944
Mike Yuan <me@yhndnzj.com>
2019-09-28 12:31:25 +0800

    Reformat code

--- btrfs-snapshot.sh
+++ btrfs-snapshot.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-num=10
+#
+# btrfs-snapshot - tool for creating btrfs snapshots
+#
 
-set_var () {
-	subvol="$1"
+usage() {
+    cat <<EOF
+usage: ${0##*/} [options]
+
+  Options:
+   -s, --subvolume <subvolume>      Subvolume for creating snapshots
+   -n, --nkeep <num>                Number of snapshots to be kept
+
+EOF
+}
+
+set_var() {
 	date="$(date -I)"
 	if [[ $subvol == / ]]; then
 		dest=/snapshot/root
@@ -14,22 +26,42 @@ set_var () {
 	fi
 }
 
-create_dest () {
+create_dest() {
 	[[ -f $dest ]] && rm "$dest"
 	[[ ! -e $dest ]] && mkdir -p "$dest"
 }
 
-create_snap () {
+create_snap() {
 	btrfs subvolume snapshot "$subvol" "$dest"/"$date"
 }
 
-delete_snap () {
-	local _num=$(( $(ls "$dest" | wc -l) - num ))
+delete_snap() {
+	local _num=$(( $(ls "$dest" | wc -l) - nkeep ))
 	mapfile -t snapshot < <(ls -d "$dest"/* | head -n $_num)
 	(( _num > 0 )) && btrfs subvolume delete "${snapshot[@]}"
 }
 
-set_var "$@"
+while :; do
+	case $1 in
+		-s|--subvolume)
+			shift
+			subvol="$1"
+			;;
+		-n|--nkeep)
+			shift
+			nkeep=$1
+			;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+	esac
+	shift
+done
+
+set_var
 create_dest
 create_snap
 delete_snap
+
+# vim: set ft=sh ts=4 sw=4 et:

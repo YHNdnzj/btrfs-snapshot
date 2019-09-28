0cd38ca97a09e121d70a703765552c0ed36dba85
Mike Yuan <me@yhndnzj.com>
2019-09-28 16:05:25 +0800

    Reformat code

--- btrfs-snapshot.sh
+++ btrfs-snapshot.sh
@@ -4,6 +4,7 @@
 #
 
 _f_parseopt=parseopt
+_i_date="$(date -I)"
 
 usage() {
     cat <<EOF
@@ -16,8 +17,7 @@ usage: ${0##*/} [options]
 EOF
 }
 
-set_var() {
-    _date="$(date -I)"
+set_dest() {
     if [[ $subvol == / ]]; then
         dest=/snapshot/root
 	else
@@ -25,18 +25,14 @@ set_var() {
     fi
 }
 
-create_snap() {
-    btrfs subvolume snapshot "$subvol" "$dest"/"$_date"
-}
-
 delete_snap() {
-    local _n_del=$(( $(ls "$dest" | wc -l) - _n_keep ))
-    local _snapshot=()
-    mapfile -t _snapshot < <(ls -d "$dest"/* | head -n $_n_del)
-    (( _n_del > 0 )) && btrfs subvolume delete "${_snapshot[@]}"
+    local _i_ndel=$(( $(ls "$dest" | wc -l) - _i_nkeep ))
+    local _d_snapshot=()
+    mapfile -t _d_snapshot < <(ls -d "$dest"/* | head -n $_i_ndel)
+    (( _i_ndel > 0 )) && btrfs subvolume delete "${_d_snapshot[@]}"
 }
 
-. parseopt
+. $_f_parseopt
 
 _opt_short='s:n:h'
 _opt_long=('subvolume:' 'nkeep:' 'help')
@@ -53,7 +49,7 @@ while :; do
             ;;
         -n|--nkeep)
             shift
-            _n_keep=$1
+            _i_nkeep=$1
             ;;
         -h|--help)
             usage
@@ -63,9 +59,9 @@ while :; do
     shift
 done
 
-set_var
+set_dest
 mkdir -p "$dest"
-create_snap
+btrfs subvolume snapshot "$subvol" "$dest"/"$_i_date"
 delete_snap
 
 # vim: set ft=sh ts=4 sw=4 et:

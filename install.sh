b630540a5bcf474de5c1bf1f2944ff8db9577fb9
Mike Yuan <me@yhndnzj.com>
2019-10-01 21:30:14 +0800

    Fix a bug

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ cd /tmp || exit 1
 wget -O btrfs-snapshot.zip https://github.com/YHNdnzj/btrfs-snapshot/archive/master.zip
 unzip btrfs-snapshot.zip
 cd btrfs-snapshot-master || exit 1
-install -Dm755 btrfs-snapshot.sh /usr/local/bin/btrfs-snapshot
 install -dm755 /usr/local/lib/btrfs-snapshot
-install -Dm644 parseopts /usr/local/lib/btrfs-snapshot/parseopts
 sed -e 's|\(^_f_parseopts\)=.*|\1=/usr/local/lib/btrfs-snapshot/parseopts|' \
     < btrfs-snapshot.sh > /usr/local/bin/btrfs-snapshot
+chmod 755 /usr/local/bin/btrfs-snapshot
+install -Dm644 parseopts /usr/local/lib/btrfs-snapshot/parseopts
 install -Dt /etc/systemd/system -m644 systemd/*
 cd ..
 rm -rf btrfs-snapshot{.zip,-master}
